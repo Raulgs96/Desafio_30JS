61f51856b82e03b0c6dc171ceb3090ee1f217fb9
unknown <manueloliverms@hotmail.com>
2024-01-14 10:08:30 -0600

    actual

--- JS/login.js
+++ JS/login.js
@@ -1,20 +1,26 @@
-let token = localStorage.getItem("token");
-const login = () => {
+import { getImageAuthor } from '../Modulos/aapi';
 
+const randomImage = async () => {
+  const image = await getImageAuthor();
 
-let mail =document.getElementById("mail").value;
-let pass = document.getElementById("password").value;
-if (mail && pass){
-  let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
-  localStorage.setItem("token", token);
-  //location.reload();
-  window.location.href = "index.html"
-}
+  return image['results'][0]['picture']['thumbnail'];
+};
 
-}
+document
+  .getElementById('login-button')
+  .addEventListener('click', async (event) => {
+    event.preventDefault(); // method that cancels the event if it is cancelable
+    localStorage.setItem(
+      'token',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    );
 
+    const getAuthor = document.getElementById('user_email');
+    const getValueAuthor = getAuthor.value.split('@')[0].toLowerCase();
+    const capitalizeFirstLetter =
+      getValueAuthor.charAt(0).toUpperCase() + getValueAuthor.slice(1);
 
-
-
-let loginButton = document.getElementById("btn-create-login");
-loginButton.addEventListener("click", login);
+    localStorage.setItem('image', await randomImage());
+    localStorage.setItem('author', capitalizeFirstLetter);
+    window.open('../index.html', '_self');
+  });
